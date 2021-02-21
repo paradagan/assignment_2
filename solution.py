8c88a6c33e5ca0126b05dabc30cc0b33a2de41d3
paradagan <78583154+paradagan@users.noreply.github.com>
2021-02-20 18:18:25 -0700

    initial

---
+++ solution.py
@@ -0,0 +1,52 @@
+#import socket module
+from socket import *
+import sys # In order to terminate the program
+
+def webServer(port=13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    #Prepare a sever socket
+    serverSocket.bind(("", port))
+    #Fill in start
+    serverSocket.listen(1)
+    #Fill in end
+
+
+    while True:
+        #Establish the connection
+        connectionSocket, addr = serverSocket.accept()
+        try:
+            message = connectionSocket.recv(1024).decode()
+            filename = message.split()[1]
+            f = open(filename[1:])
+            outputdata = f.readlines()
+
+            #Send one HTTP header line into socket
+            #Fill in start
+            connectionSocket.send('HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode())
+            #Fill in end
+
+            # Send the content of the requested file to the client
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
+            break
+        except IOError:
+            #Send response message for file not found (404)
+            #Fill in start
+            connectionSocket.send('HTTP/1.1 404 Not Found\n\n'.encode())
+            #Fill in end
+
+            #Close client socket
+            #Fill in start
+            connectionSocket.close()
+            break
+            #Fill in end
+
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+    webServer(13331)
